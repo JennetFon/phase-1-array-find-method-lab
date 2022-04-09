9e91d6ed0feedb9c8c4241572f1c8fc2d2577ce2
Jennet <mankahjane2019@gmail.com>
2022-04-09 08:14:12 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function superbowlWin() {
+    const record = [
+    {year: "2018", result: "L"}
+
+     ]
+     const year = record.find( ({ year }) => year === '2018' );
+     console.log(year) // {year: "2018"}
+
+  const result = record.find( ({result}) => result === "L" );
+console.log(result) // {result: "L"}
+    }
+    
+    function superbowlWin() {
+    const record = [
+       {year:'2015', results: "W"}
+       console.log(result) // {"W"}
+    ]
+    }  
+    function superbowlWin() {
+        const sadReality =  [ { result: "N/A" }, { result: "N/A" }
+        {year:'2015', results: " "}
+    ]
+    console.log(sadReality) // const sadReality =  [ { result: "N/A" }, { result: "N/A" };
+    }
+
+    constsuperbowlWin1() {
+        
+    }
